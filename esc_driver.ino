f7ca6204a1b213af50e84e48ff426c2a4cb7ec25
Peter Novotnak <pnovotnak@flexion.us>
2014-06-28 02:40:15 -0500

    everything functional, sweep, etc. Debuging turned off

--- esc_driver.ino
+++ esc_driver.ino
@@ -1,7 +1,7 @@
-#include <Servo.h> 
- 
-boolean debug   = true;
- 
+#include <Servo.h>
+
+boolean debug   = false;
+
 Servo ESC;  // create servo object to control the ESC
             // a maximum of eight servo objects can be created
 
@@ -11,89 +11,105 @@ int esc_pin      = 2;
 int throttle_pos = 0;
 
 
-int pos         = 50;    // variable to store the servo position (don't start at zero)
+int pos         = 0;    // variable to store the servo position (don't start at zero)
 int throttle    = 0;
 
-int output_min  = 50;
-int output_max  = 180;
+int output_min  = 35;
+int output_max  = 160;
 
-int throttle_min = 65;
+int throttle_min = 90;
 int throttle_max = 950;
 
-int sweep_delay = 15;
+int sweep_delay = 12;
 int sweep_min   = 0;
 int sweep_max   = output_max;
+int sweep_step  = 2;
 
-int throttle_normalized;
+int mapped_throttle;
 int stale_throttle;
 
+
+
+/*
+ * Initialization function needed with some ESCs
+*/
 void sweep() {
-  if (debug) {Serial.println("beginning sweep");}
-  for(pos = sweep_min; pos < sweep_max; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree
-    ESC.write(pos);               // tell servo to go to position in variable 'pos'
+  if (debug) Serial.println("beginning sweep");
+  for(pos = sweep_min; pos < sweep_max; pos += sweep_step) {
+    ESC.write(pos);
     if (debug) {Serial.print("sweeping up.. ");Serial.println(pos);}
-    delay(sweep_delay);           // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = sweep_max; pos >= sweep_min; pos-=1)  // goes from 180 degrees to 0 degrees 
-  {     
-    ESC.write(pos);               // tell servo to go to position in variable 'pos' 
+    delay(sweep_delay);
+  }
+  for(pos = sweep_max; pos >= sweep_min; pos-=sweep_step) {
+    ESC.write(pos);
     if (debug) {Serial.print("sweeping down.. "); Serial.println(pos);}
-    delay(sweep_delay);           // waits 15ms for the servo to reach the position
-  } 
+    delay(sweep_delay);
+  }
 }
 
 
+/*
+ * Normalzie a throttle input, to avoid crazyness from a dirty pot.
+*/
 int normalize_throttle(int throttle_val, int last_throttle) {
-  if (throttle_val > throttle_max) {
-    throttle_val = throttle_max;
-  }
-  
-  if (throttle_val < throttle_min) {
-    throttle_val = throttle_min;
-  }
-  
+
+  // Change throttle
   if (throttle_val > last_throttle) {
-    throttle_val = last_throttle + 1;
+    throttle_val = last_throttle + 2;
+
   }
 
   if (throttle_val < last_throttle) {
-    throttle_val = last_throttle - 1;
+    throttle_val = last_throttle - 4;
+  }
+
+  // Keep the throttle within bounds
+  if (throttle_val > output_max) {
+    throttle_val = output_max;
+  }
+
+  if (throttle_val < output_min) {
+    throttle_val = output_min;
   }
 
   return throttle_val;
 }
 
+
+
 void setup() {
-  if (debug) {Serial.begin(9600);}
-  ESC.attach(esc_pin);  // attaches the servo on pin 9 to the servo object
-  if (debug) {Serial.println("attached to ESC");}
+  if (debug) Serial.begin(9600);
+  ESC.attach(esc_pin);
   ESC.write(pos);
-  delay(4000);
-  //sweep();
+  sweep();
+  delay(500);
 }
 
 
+
 void loop() {
-  if (debug) {
-    //Serial.println("loop iteration");
-  }
+  //if (debug) Serial.println("loop iteration");
+
   throttle = analogRead(throttle_pin);
 
-  Serial.println(throttle);
+  //if (debug) Serial.println(throttle);
 
-  throttle_normalized = map(
-                            throttle,
-                            throttle_min, throttle_max,
-                            output_min, output_max
-                        );
-  
-  throttle = normalize_throttle(throttle, stale_throttle);
-  
-  //Serial.println(throttle_normalized);
+  mapped_throttle = map(
+                        throttle,
+                        throttle_min,
+                        throttle_max,
+                        output_min,
+                        output_max
+                    );
 
-  ESC.write(throttle_normalized);
-  stale_throttle = throttle_normalized;
-  
+  throttle = normalize_throttle(mapped_throttle, stale_throttle);
+
+  if (debug) {Serial.print(mapped_throttle);Serial.print(" - ");Serial.println(throttle);}
+
+  if (stale_throttle != throttle) {
+    ESC.write(throttle);
+  }
+
+  stale_throttle = throttle;
   delay(15);
 }
